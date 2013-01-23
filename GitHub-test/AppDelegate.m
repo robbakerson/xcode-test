9d77d88539c478827d20cda875d1017e365417b0
Robb Akerson <robb.akerson@target.com>
2013-01-23 09:19:00 -0600

    second commit

--- GitHub-test/AppDelegate.m
+++ GitHub-test/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //comment.
     // Override point for customization after application launch.
     return YES;
 }
